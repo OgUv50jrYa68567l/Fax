d43485a6fd7ce86f3401f38504c55502d3dfb57c
OgUv50jrYa68567l <9nwfbsezdogcst@atomicmail.io>
2026-01-31 11:51:56 -0400

    .

---
+++ Place Loader.lua
@@ -0,0 +1,8 @@
+local Games = {
+    [93978595733734] = "https://raw.githubusercontent.com/OgUv50jrYa68567l/Fax/refs/heads/main/Violence%20District",
+}
+
+local id = game.PlaceId
+if Games[id] then
+    loadstring(game:HttpGet(Games[id]))()
+end
